e8457880f0b89a1da3cb442dca0173d157de0871
shinnosukeono <shinnosuke-ono211@email.plala.or.jp>
2023-10-29 08:20:08 +0000

    processor debuggin

--- core/datapath.sv
+++ core/datapath.sv
@@ -33,12 +33,38 @@ module datapath (
 
     // pc logic
     flopr #(.DATAW(32)) pcreg(clk, reset, pc_next, pc);
-    adder #(.DATAW(32)) pc_add4(pc, 32'd4, pc_plus4);
-    adder #(.DATAW(32)) pc_add_branch(pc, imm_ext, pc_target);
+    adder #(
+            .DATAW(32)
+        ) pc_add4 (
+            .a(pc),
+            .b(32'd4),
+            .c_in(1'b0),
+            .s(pc_plus4),
+            .c_out()
+        );
+    adder #(
+            .DATAW(32)
+        ) pc_add_branch (
+            .a(pc),
+            .b(imm_ext),
+            .c_in(1'b0),
+            .s(pc_target),
+            .c_out()
+        );
     mux #(.DATAW(32)) pc_mux({pc_target, pc_plus4}, pc_src, pc_next);
 
     // register file logic
-    regfile rf(clk, reg_write, instr[19:15], instr[24:20], instr[11:7], result, src_a, write_data);
+    regfile rf (
+        .clk(clk),
+        .rst(reset),
+        .we3(reg_write),
+        .a1(instr[19:15]),
+        .a2(instr[24:20]),
+        .a3(instr[11:7]),
+        .wd3(result),
+        .rd1(src_a),
+        .rd2(write_data)
+    );
     extend ext(instr[31:7], imm_src, imm_ext);
 
     // control register file logic 
@@ -48,7 +74,7 @@ module datapath (
     mux #(.DATAW(32)) c_reg_data_mux({32'(instr[19:15]), src_a}, c_reg_src, c_reg_data_in);
 
     // alu logic
-    mux #(.DATAW(32)) src_b_mux({write_data, imm_ext}, alu_src, src_b);
+    mux #(.DATAW(32)) src_b_mux({imm_ext, write_data}, alu_src, src_b);
     alu alu(src_a, src_b, alu_control, alu_result, negative_flag, zero_flag, carry_out_flag, overflow_flag);
     mux4 #(.DATAW(32)) result_mux({c_reg_data_out, pc_plus4, read_data, alu_result}, result_src, result);
 endmodule

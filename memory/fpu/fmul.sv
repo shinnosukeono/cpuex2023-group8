1ef713f6a91004a58680d8df9a3219a97c310f45
YMikami <ymikami1212@Ubuntu.myguest.virtualbox.org>
2023-11-22 17:38:46 +0900

    fmul修正

--- memory/fpu/fmul.sv
+++ memory/fpu/fmul.sv
@@ -26,18 +26,24 @@ module fmul
     wire [25:0] hxhy = hx * hy;
     wire [23:0] hxly = hx * ly;
     wire [23:0] hylx = hy * lx;
-    wire [8:0] e_res_unshifted = ex + ey - 127;
-    wire s_res = sx ^ sy;
+    wire [9:0] e_res_unshifted = ex + ey - 127;
+    wire temp_s_res = sx ^ sy;
 
     wire [25:0] m_res_long = hxhy + (hxly >> 11) + (hylx >> 11) + 2;
-    wire [8:0] e_res_shifted = e_res_unshifted + 1;
+    wire [9:0] e_res_shifted = e_res_unshifted + 1;
 
-    wire [7:0] e_res = (e_res_unshifted[8]) ? 8'b0 :
-                       (m_res_long[25])     ? e_res_shifted :
-                                              e_res_unshifted;
-    wire [22:0] m_res = (~(|e_res))      ? 23'b0 : // e_res == 0
+    wire [7:0] e_res = m_res_long[25] ? (e_res_shifted[9] ? 8'b0 :           // e_res_shifted < 0
+                                         e_res_shifted[8] ? 8'hff :          // e_res_shifted > 255
+                                                            e_res_shifted) :
+                                        (e_res_unshifted[9] ? 8'b0 :         // e_res_unshifted < 0
+                                         e_res_unshifted[8] ? 8'hff :        // e_res_unshifted > 255
+                                                              e_res_unshifted);
+    wire is_zero = ~(|e_res);
+    wire ovf = &e_res;
+    wire [22:0] m_res = (is_zero | ovf)  ? 23'b0 : // e_res == 0
                         (m_res_long[25]) ? m_res_long[24-:23] :
                                            m_res_long[23-:23];
+    wire s_res = is_zero ? 1'b0 : temp_s_res;
     assign res = {s_res,e_res,m_res};
 
 endmodule

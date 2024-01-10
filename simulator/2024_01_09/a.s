ab0ac8b37825b774eeee053b15a74bea5944ede4
Masahito Tsukahara <tsuka7903110@icloud.com>
2024-01-10 15:19:55 +0900

    ok0110

--- simulator/2024_01_09/a.s
+++ simulator/2024_01_09/a.s
@@ -264,7 +264,7 @@ caml_main:                              # @caml_main
 	lw	a0, %lo(size)(a0)
 	lui	a1, %hi(dbg)
 	lw	a1, %lo(dbg)(a1)
-	li	a2, 4
+	li	a2, 256
 	sw	a2, 0(a0)
 	sw	a2, 4(a0)
 	sb	zero, 0(a1)
